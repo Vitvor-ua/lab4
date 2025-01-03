13bb817adf6431569d7ed04277be0f3b88541a93
Vitvor <112934325+Vitvor-ua@users.noreply.github.com>
2025-01-03 20:51:20 +0200

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"lab4","l":"Line"},{"p":"lab4","l":"LineComposite"},{"p":"lab4","l":"Point"},{"p":"lab4","l":"PointAdapter"},{"p":"lab4","l":"PointPrimitive"}];updateSearchResults();
